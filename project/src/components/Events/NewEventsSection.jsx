0402c5129a324f56604dab5243feccc3c80f64e4
gangbeomsu <kbs21c1987@gmail.com>
2023-12-19 18:14:08 +0900

    staleTime과 gcTime 알아보기

--- project/src/components/Events/NewEventsSection.jsx
+++ project/src/components/Events/NewEventsSection.jsx
@@ -9,10 +9,14 @@ export default function NewEventsSection() {
   // queryKey는 쿼리를 식별하는 문자열이다. *필수
   // queryKey는 기본적으로 배열이다.
   // queryKey가 변경되면 useQuery는 새로운 데이터를 가져온다.
+  // staleTime은 쿼리가 만료되기 전까지 캐시된 데이터를 사용한다. 기본값은 0, 단위는 밀리초
+  // gcTime은 쿼리가 만료된 후에 캐시에서 제거된다. 기본값은 5분, 단위는 밀리초
 
   const { data, isPending, isError, error } = useQuery({
     queryKey: ['events'],
     queryFn: fetchEvents,
+    staleTime: 5000,
+    // gcTime: 1000,
   });
   console.log(data);
   let content;
