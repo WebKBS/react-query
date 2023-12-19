c33de7977baf882d0bc38c87c87a1c065ab58990
gangbeomsu <kbs21c1987@gmail.com>
2023-12-19 18:34:03 +0900

    Search 기능 구현하기

--- project/src/components/Events/FindEventSection.jsx
+++ project/src/components/Events/FindEventSection.jsx
@@ -1,10 +1,51 @@
-import { useRef } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { useRef, useState } from 'react';
+import { fetchEvents } from '../../util/http';
+import ErrorBlock from '../UI/ErrorBlock';
+import LoadingIndicator from '../UI/LoadingIndicator';
+import EventItem from './EventItem';
 
 export default function FindEventSection() {
   const searchElement = useRef();
+  const [searchTerm, setSearchTerm] = useState('');
+
+  // 이 컴포넌트의 events의 키는 NewEventsSection의 queryKey와 독립적으로 사용된다.
+  // 변경되는 queryKey가 있다면 {}를 사용하여 쿼리를 식별하는 문자열을 전달한다.
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ['events', { search: searchTerm }],
+    queryFn: () => fetchEvents(searchElement.current.value),
+  });
 
   function handleSubmit(event) {
     event.preventDefault();
+    setSearchTerm(searchElement.current.value);
+  }
+
+  let content = <p>Please enter a search term and to find events.</p>;
+
+  if (isPending) {
+    content = <LoadingIndicator />;
+  }
+
+  if (isError) {
+    content = (
+      <ErrorBlock
+        title="An error occurered"
+        message={error.info?.message || 'Faild to fetch events.'}
+      />
+    );
+  }
+
+  if (data) {
+    content = (
+      <ul className="events-list">
+        {data.map((event) => (
+          <li key={event.id}>
+            <EventItem event={event} />
+          </li>
+        ))}
+      </ul>
+    );
   }
 
   return (
@@ -20,7 +61,7 @@ export default function FindEventSection() {
           <button>Search</button>
         </form>
       </header>
-      <p>Please enter a search term and to find events.</p>
+      {content}
     </section>
   );
 }
