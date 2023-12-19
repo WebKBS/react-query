2082142aa20c2849df729b3e01b7767fa187011a
gangbeomsu <kbs21c1987@gmail.com>
2023-12-19 21:14:23 +0900

    post 성공후 path 변경과 데이터 변경 반영하기

--- project/src/components/Events/NewEvent.jsx
+++ project/src/components/Events/NewEvent.jsx
@@ -1,7 +1,7 @@
 import { Link, useNavigate } from 'react-router-dom';
 
 import { useMutation } from '@tanstack/react-query';
-import { createNewEvent } from '../../util/http.js';
+import { createNewEvent, queryClient } from '../../util/http.js';
 import ErrorBlock from '../UI/ErrorBlock.jsx';
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
@@ -11,6 +11,11 @@ export default function NewEvent() {
 
   const { mutate, isPending, isError, error } = useMutation({
     mutationFn: createNewEvent,
+    // onSuccess는 mutate가 성공적으로 완료되면 호출된다.
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['events'] }); // 데이터가 변경되었음을 알리고 쿼리를 다시 가져온다.
+      navigate('../');
+    },
   });
 
   function handleSubmit(formData) {
