b59c3333bbf256ac8e6f68a468f7a3cb9e88279d
gangbeomsu <kbs21c1987@gmail.com>
2023-12-20 18:31:13 +0900

    onMutate를 사용해서 edit 페이지 구현하기

--- project/src/components/Events/EditEvent.jsx
+++ project/src/components/Events/EditEvent.jsx
@@ -1,7 +1,7 @@
 import { Link, useNavigate, useParams } from 'react-router-dom';
 
-import { useQuery } from '@tanstack/react-query';
-import { fetchEvent } from '../../util/http.js';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { fetchEvent, queryClient, updateEvent } from '../../util/http.js';
 import ErrorBlock from '../UI/ErrorBlock.jsx';
 import LoadingIndicator from '../UI/LoadingIndicator.jsx';
 import Modal from '../UI/Modal.jsx';
@@ -11,12 +11,34 @@ export default function EditEvent() {
   const navigate = useNavigate();
   const params = useParams();
 
+  const { mutate } = useMutation({
+    mutationFn: updateEvent,
+    onMutate: async (data) => {
+      const newEvent = data.event;
+
+      await queryClient.cancelQueries({ queryKey: ['events', params.id] }); // 쿼리를 취소한다.
+      const previousEvent = queryClient.getQueryData(['events', params.id]); // 이전 데이터를 가져온다.
+      queryClient.setQueryData(['events', params.id], newEvent); // 쿼리 데이터를 업데이트한다.
+
+      return { previousEvent };
+    },
+    onError: (error, data, context) => {
+      queryClient.setQueryData(['events', params.id], context.previousEvent); // 쿼리 데이터를 복원한다.
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ['events', params.id] }); // 데이터가 변경되었음을 알리고 쿼리를 다시 가져온다.
+    },
+  });
+
   const { data, isPending, isError, error } = useQuery({
     queryKey: ['events', params.id],
     queryFn: ({ signal }) => fetchEvent({ signal, id: params.id }),
   });
 
-  function handleSubmit(formData) {}
+  function handleSubmit(formData) {
+    mutate({ id: params.id, event: formData });
+    navigate('../');
+  }
 
   function handleClose() {
     navigate('../');
